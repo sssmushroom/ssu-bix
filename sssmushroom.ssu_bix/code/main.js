107a6af7e2a4bb0574dd0e4d5ab49bd1982bc3b3
bbvch13531 <bbvch13531@gmail.com>
2019-11-23 14:11:19 +0900

    FEAT: Setup thumbnail-card view
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

--- sssmushroom.ssu_bix/code/main.js
+++ sssmushroom.ssu_bix/code/main.js
@@ -16,10 +16,20 @@ module.exports.function = function main (input) {
     */
   // print(input)
 
-  var test1 = {
-    "office": "스파르탄 교육원",
+  var test1 = [
+    {"office": "스파르탄 교육원",
     "title": "글로벌 SW 인턴십 프로그램",
-    "url": "https://fun.ssu.ac.kr/ko/program/all/view/1310"
-  }
+    "url": "https://fun.ssu.ac.kr/ko/program/all/view/1310",
+    "thumbnailImage": "https://fun.ssu.ac.kr/attachment/view/43396/cover.png",
+    "date": "2019.11.22(금) ~ 2019.12.20(금)"
+    },
+    
+    {"office": "교육과정혁신센터",
+    "title": "두드림학기 공모전",
+    "url": "https://fun.ssu.ac.kr/ko/program/all/view/1285",
+    "thumbnailImage": "https://fun.ssu.ac.kr/attachment/view/42901/cover.png",
+    "date": "2019.11.22(금) 09:00 ~ 2019.12.20(금) 09:00"
+    }
+  ]
   return test1
 }
