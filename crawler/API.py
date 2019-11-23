51f257b805873559fcc89375af62b590378435d0
queragion2726 <jms100300@naver.com>
2019-11-23 17:11:22 +0900

    Add CSE notice API and parser

--- crawler/API.py
+++ crawler/API.py
@@ -1,9 +1,12 @@
 from flask import Flask, send_from_directory
 from flask_restful import Resource, Api
 from crawler import Crawler
+
 from fun_parser import PARSER_DATE as FUN_DATE_PARSER
 from fun_parser import PARSER_APPROACH as FUN_APPROACH_PARSER
 from fun_parser import PARSER_APPLICANT as FUN_APPLICANT_PARSER
+
+from cse_parser import PARSER as CSE_PARSER
 
 app = Flask(__name__)
 api = Api(app)
@@ -23,9 +26,15 @@ class FunApplicantGET(Resource):
         it = Crawler(FUN_APPLICANT_PARSER)
         return it.parse()
 
+class CSENoticeGET(Resource):
+    def get(self):
+        it = Crawler(CSE_PARSER)
+        return it.parse()
+
 api.add_resource(FunApproachGET, '/fun/approach')
 api.add_resource(FunApplicantGET, '/fun/applicant')
 api.add_resource(FunDateGET, '/fun/date')
+api.add_resource(CSENoticeGET, '/cse/notice')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081, debug=False)
---
+++ crawler/cse_parser.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+from flask import jsonify
+import re
+
+class CSEParser:
+    BASE_URL = 'http://cse.ssu.ac.kr/'
+    TARG_URL = 'http://cse.ssu.ac.kr/main/index.htm'
+
+    def parse(self, soup):
+        target_div = soup.find('div', id='notice_list')
+        target_list = target_div.select('ul > li')
+
+        ret = []
+
+        for item in target_list:
+            cur_dict = dict()
+
+            a_node = item.find('a')
+            onclick_txt = a_node['onclick']
+            onclick_split = re.split(',|\(|\)', onclick_txt)
+
+            no = onclick_txt[1]
+            url = onclick_txt[2][4:-1]
+            opt = onclick_split[3][1:-1]
+
+            # original format of onclick
+            # onclick="viewContents(4224,'../03_sub/01_sub.htm','&bbs_cmd=view&page=1&key=&keyfield=&bbs_code=Ti_BBS_1');return false;"
+            
+            cur_dict['URL'] = self.BASE_URL + url + '?no=' + no + opt
+
+            cur_dict['title'] = a_node.text
+            cur_dict['date'] = item.find('span', class_='list_date').text
+
+            ret.append(cur_dict)
+        return ret
+
+PARSER = CSEParser()
