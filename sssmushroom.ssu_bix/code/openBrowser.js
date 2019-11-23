f28afd85842a291bec2e7c977544302beaa08e94
bbvch13531 <bbvch13531@gmail.com>
2019-11-23 14:50:47 +0900

    FEAT: Impl open browser app launch
    Signed-off-by: bbvch13531 <bbvch13531@gmail.com>

---
+++ sssmushroom.ssu_bix/code/openBrowser.js
@@ -0,0 +1,8 @@
+module.exports.function = function openBrowser (url) {
+  // const console = require('console');
+  // console.log(url);
+  // console.log(tel);
+  
+  let result = url;
+  return result;
+}
